72df9953f556b36c0af604adeacfc777a57b61fa
thijsvl <t.vanlankveld@esciencecenter.nl>
2024-06-27 13:41:32 +0200

    Added initial implementations for "weighted" and "multinomial" link deletion.

--- dgl_ptm/dgl_ptm/network/link_deletion.py
+++ dgl_ptm/dgl_ptm/network/link_deletion.py
@@ -4,14 +4,23 @@ import torch
 def link_deletion(agent_graph, method: str, threshold: float):
     '''
         link_deletion - deletes links between agents according to a selected deletion method.
+
+        In case of the "weighted" and "multinomial" methods, the probability of
+        selecting an edge for deletion is (multiplied by) the inverse weight of that edge.
         
         Args:
             agent_graph: DGLGraph with agent nodes and edges connecting agents
-            method: deletion method. Either
-                "probability" (each edge selected idependently with this probability) or
-                "size" (fixed number of randomly selected edges)
+            method: deletion method. Must be either
+                "probability": each edge selected idependently with equal probability,
+                "weighted": each edge selected independently with weighted probability,
+                "size": fixed number of edges selected with equal probability, or
+                "multinomial": fixed number of edges selected with weighted probability.
             threshold: Threshold for deleting an existing edge between two agent nodes.
-                Either the probability each edge is deleted or the number of edges to delete.
+                The interpretation of this threshold depends on the deletion method:
+                "probability": the probability for deleting any edge,
+                "weighted": the base probability for deleting any edge,
+                "size": the number of edges to delete, or
+                "multinomial": the number of edges to delete.
 
         Output:
             agent_graph: Updated agent_graph with reduced edges based on 'method' and 'threshold'.
@@ -23,25 +32,39 @@ def _select_edges(agent_graph, method: str, threshold: float):
     '''
         Identify edges to delete according to a selected deletion method.
 
+        In case of the "weighted" and "multinomial" methods, the probability of
+        selecting an edge for deletion is (multiplied by) the inverse weight of that edge.
+        
         Args:
             agent_graph: DGLGraph with agent nodes and edges connecting agents
-            method: deletion method. Either
-                "probability" (each edge selected idependently with this probability) or
-                "size" (fixed number of randomly selected edges)
+            method: deletion method. Must be either
+                "probability": each edge selected idependently with equal probability,
+                "weighted": each edge selected independently with weighted probability,
+                "size": fixed number of edges selected with equal probability, or
+                "multinomial": fixed number of edges selected with weighted probability.
             threshold: Threshold for deleting an existing edge between two agent nodes.
-                Either the probability each edge is deleted or the number of edges to delete.
+                The interpretation of this threshold depends on the deletion method:
+                "probability": the probability for deleting any edge,
+                "weighted": the base probability for deleting any edge,
+                "size": the number of edges to delete, or
+                "multinomial": the number of edges to delete.
 
         Return:
             agent_graph.edge_ids: edge_ids for agent edges to be deleted
     '''
     upper_triangular = _sparse_upper_triangular(agent_graph.adj())
 
-    # TODO: check https://discuss.pytorch.org/t/torch-equivalent-of-numpy-random-choice/16146/16
     mask_edges
     if method == "probability":
-        mask_edges = torch.rand(upper_triangular.val.size()[0]) < threshold # * triu_adj.val TODO: Is this needed?
+        mask_edges = torch.rand(upper_triangular.val.size()[0]) < threshold
+    elif method == "weighted":
+        mask_edges = ((1.-agent_graph.edata['weight']) * torch.rand(upper_triangular.val.size()[0])) < threshold
     elif method == "size":
-        mask_edges = torch.randperm(upper_triangular.val.size()[0]) < threshold # * triu_adj.val TODO: Is this needed?
+        mask_edges = torch.randperm(upper_triangular.val.size()[0]) < threshold
+    elif method == "multinomial":
+        mask_edges = torch.zeros(upper_triangular.val.size()[0])
+        eid = (1.-agent_graph.edata['weight']).multinomial(threshold, replacement=False)
+        mask_edges.scatter_(0, eid, 1.)
     else:
         raise NotImplementedError('Currently only "probability" and "size" deletion methods are supported')
         mask_edges = torch.zeros(upper_triangular.val.size()[0])
