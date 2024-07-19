1a9635848f8057ebc74220cf84a4c96ff6a397e7
SarahAlidoost <f.alidoost@esciencecenter.nl>
2024-07-19 11:28:16 +0200

    remove code and tests related to degree distribution

--- dgl_ptm/dgl_ptm/util/network_metrics.py
+++ dgl_ptm/dgl_ptm/util/network_metrics.py
@@ -1,8 +1,5 @@
 """This module contains functions for computing network metrics."""
 
-from scipy import stats
-from collections import Counter
-
 def mean_connectivity(graph):
     """
     Compute the mean connectivity of a graph.
@@ -12,46 +9,3 @@ def mean_connectivity(graph):
     return: float: The mean connectivity.
     """
     return graph.number_of_edges() / graph.number_of_nodes()
-
-
-def degree_distribution(graph):
-    """
-    Compute the degree distribution of a graph.
-    The degree distribution is a probability distribution of the degrees over
-    all nodes in the network.
-
-    param: graph: dgl_ptm Graph
-    return: dict: The degree distribution.
-    """
-    graph = graph.to_networkx()
-    degrees = [degree for node, degree in graph.degree()]
-    degree_counts = Counter(degrees)
-    total_nodes = len(degrees)
-    distribution = {
-        degree: count / total_nodes for degree, count in degree_counts.items()
-        }
-    return distribution
-
-
-def modal_degree(graph):
-    """
-    Compute the modal degree of a graph.
-    It finds the degree that appears most frequently in the degree distribution.
-
-    param: graph: dgl_ptm Graph
-    return: int: The modal degree.
-    """
-    distribution = degree_distribution(graph)
-    return max(distribution, key=distribution.get)
-
-
-def skewness_of_degree_distribution(graph):
-    """
-    Compute the skewness of the degree distribution of a graph.
-
-    param: graph: dgl_ptm Graph
-    return: float: The skewness of the degree distribution.
-    """
-    graph = graph.to_networkx()
-    degrees = [degree for node, degree in graph.degree()]
-    return stats.skew(degrees)
--- dgl_ptm/tests/test_util.py
+++ dgl_ptm/tests/test_util.py
@@ -3,9 +3,6 @@ import dgl_ptm
 import os
 
 from dgl_ptm.util.network_metrics import mean_connectivity
-from dgl_ptm.util.network_metrics import degree_distribution
-from dgl_ptm.util.network_metrics import modal_degree
-from dgl_ptm.util.network_metrics import skewness_of_degree_distribution
 
 os.environ["DGLBACKEND"] = "pytorch"
 
@@ -26,31 +23,3 @@ class TestNetworkMetrics:
         mc = mean_connectivity(model.model_graph)
         assert mc is not None
         assert isinstance(mc, float)
-
-    def test_degree_distribution(self, model):
-        dd = degree_distribution(model.model_graph)
-        assert dd is not None
-
-        model.step() # timestep 1
-        dd = degree_distribution(model.model_graph)
-        assert dd is not None
-        assert isinstance(dd, dict)
-
-
-    def test_modal_degree(self, model):
-        md = modal_degree(model.model_graph)
-        assert md is not None
-
-        model.step()
-        md = modal_degree(model.model_graph)
-        assert md is not None
-        assert isinstance(md, int)
-
-    def test_skewness_of_degree_distribution(self, model):
-        sd = skewness_of_degree_distribution(model.model_graph)
-        assert sd is not None
-
-        model.step()
-        sd = skewness_of_degree_distribution(model.model_graph)
-        assert sd is not None
-        assert isinstance(sd, float)
