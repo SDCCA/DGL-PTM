5ee5ba1587c817681b97f4794864a78d731db4a2
SarahAlidoost <f.alidoost@esciencecenter.nl>
2024-08-05 15:13:47 +0200

    refactor test_average_degree_initialize with exact value of average_degree

--- dgl_ptm/tests/test_util.py
+++ dgl_ptm/tests/test_util.py
@@ -9,16 +9,19 @@ os.environ["DGLBACKEND"] = "pytorch"
 @pytest.fixture
 def model():
     model = dgl_ptm.PovertyTrapModel(model_identifier='my_model')
-    model.set_model_parameters()
+    # to make sure the results are reproducible
+    model.set_model_parameters(
+        initial_graph_args={'seed': 100, 'new_node_edges': 1},
+        number_agents=100,
+        initial_graph_type="barabasi-albert")
     model.initialize_model()
     return model
 
 
 class TestNetworkMetrics:
     def test_average_degree_initialize(self, model):
-        ad = average_degree(model.model_graph)
-        assert model.average_degree == ad
-        assert isinstance(ad, float)
+        expected_ad = 1.98
+        assert model.average_degree == expected_ad
 
     def test_average_degree_step(self, model):
         model.step() # timestep 1
