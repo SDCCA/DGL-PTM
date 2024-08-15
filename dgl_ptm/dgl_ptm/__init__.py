ddb01cda8213bcea4b1049855060df17412a2ef9
SarahAlidoost <f.alidoost@esciencecenter.nl>
2024-08-15 16:00:19 +0200

    fix email address in init

--- dgl_ptm/dgl_ptm/__init__.py
+++ dgl_ptm/dgl_ptm/__init__.py
@@ -13,5 +13,5 @@ from dgl_ptm import config
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Team Atlas"
-__email__ = "p.chandramouli@esciencecenter.nl"
+__email__ = "m.grootes@esciencecenter.nl"
 __version__ = "0.1.0"
