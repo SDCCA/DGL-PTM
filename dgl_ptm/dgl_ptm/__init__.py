1f9e1a241c8237082be923332ad88e5078992141
SarahAlidoost <f.alidoost@esciencecenter.nl>
2024-08-15 17:24:40 +0200

    fix linter errors of init

--- dgl_ptm/dgl_ptm/__init__.py
+++ dgl_ptm/dgl_ptm/__init__.py
@@ -1,14 +1,17 @@
-"""Documentation about dgl_ptm"""
+"""Documentation about dgl_ptm."""
 import logging
 
-#from dgl_ptm.model import initialize_model, step
-from dgl_ptm.model.initialize_model import PovertyTrapModel
 # from dgl_ptm.agent import agent_update
 # from dgl_ptm.agentInteraction import trade_money
 # from dgl_ptm.network import global_attachment, link_deletion
 # from dgl_ptm.model import initialize_model, data_collection, step
 # from dgl_ptm.util import *
 from dgl_ptm import config
+
+#from dgl_ptm.model import initialize_model, step
+from dgl_ptm.model.initialize_model import PovertyTrapModel
+
+__all__ = ['config', 'PovertyTrapModel']
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
