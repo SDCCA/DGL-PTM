42800bddac060a38e65824670f85871775687c28
Victoria <Victoria.Garibay@gmail.com>
2026-04-19 23:08:05 +0200

    changed environmental variable CONDA_HOME to CONDA_PREFIX

--- Experiments/AdditionalExperiments/default_reconstruction_run.sh
+++ Experiments/AdditionalExperiments/default_reconstruction_run.sh
@@ -20,7 +20,7 @@ log_file="default_run_times.log"
 > "$log_file" 
 
 # Environment (Snellius specific)
-source ${CONDA_HOME}/etc/profile.d/conda.sh
+source ${CONDA_PREFIX}/etc/profile.d/conda.sh
 #conda env create -f ../environment.yml --name dgl_ptm_gpu
 conda activate dgl_ptm_gpu
 
--- Experiments/AdditionalExperiments/disruption_run_4f3.sh
+++ Experiments/AdditionalExperiments/disruption_run_4f3.sh
@@ -13,7 +13,7 @@ log_file="default_disrupt4_f3_run_times.log"
 > "$log_file" 
 
 # Environment (Snellius specific)
-source ${CONDA_HOME}/etc/profile.d/conda.sh
+source ${CONDA_PREFIX}/etc/profile.d/conda.sh
 #conda env create -f ../environment.yml --name dgl_ptm_gpu
 conda activate dgl_ptm_gpu
 
